3f4c0fba12abc0b843d374856047bb4053ae864a
Jason <Jaytonbye@yahoo.com>
2022-01-29 09:20:36 -0500

    bracket builder is complete

--- src/server/bracketing_logic/binarysplitting.js
+++ src/server/bracketing_logic/binarysplitting.js
@@ -32,66 +32,72 @@ people = [
   { name: "Wrestler Seeded31", team: "Team 31", seed: 31 },
   { name: "Wrestler Seeded32", team: "Team 32", seed: 32 },
 ];
-let bigArray = [];
-let binarySplitting = (peopleArray) => {
-  let topArray = [];
-  let botArray = [];
 
-  for (x = 0; x < peopleArray.length; x++) {
-    if (x % 4 === 0 || x % 4 === 3) {
-      topArray.push(peopleArray[x]);
-    } else {
-      botArray.push(peopleArray[x]);
-    }
-  }
+let handleSeedingFunction = (initialArrayOfCompetitorsOrderedBySeed) => {
+  let bigArray = [];
+  let binarySplitting = (initialArrayOfCompetitorsOrderedBySeed) => {
+    let topArray = [];
+    let botArray = [];
 
-  if (botArray.length === 2) {
-    bigArray.push(topArray);
-    bigArray.push(botArray);
-
-    return;
-  } else {
-    binarySplitting(topArray);
-    binarySplitting(botArray);
-  }
-};
-binarySplitting(people);
-let bo2Array = [];
-let newBiggestArray = [];
-
-for (let a = 0; a < bigArray.length / 2; a++) {
-  bo2Array.unshift(bigArray[a + bigArray.length / 2]);
-}
-for (let a = 0; a < bigArray.length / 2; a++) {
-  newBiggestArray.push(bigArray[a]);
-}
-for (let a = 0; a < bigArray.length / 2; a++) {
-  newBiggestArray.push(bo2Array[a]);
-}
-
-let extraNewBiggestArray = [];
-
-for (let x = 0; x < newBiggestArray.length; x++) {
-  extraNewBiggestArray.push(newBiggestArray[x][0]);
-
-  extraNewBiggestArray.push(newBiggestArray[x][1]);
-}
-
-console.log(extraNewBiggestArray);
-
-let makeSureOddSeedsAreOnTopFunction = (
-  formattedArrayOfWrestlersTeamsAndSeeds
-) => {
-  for (x = 0; x < formattedArrayOfWrestlersTeamsAndSeeds.length; x++) {
-    if (x % 2 === 0) {
-      if (formattedArrayOfWrestlersTeamsAndSeeds[x].seed % 2 === 0) {
-        let placeholder1 = formattedArrayOfWrestlersTeamsAndSeeds[x];
-        let placeholder2 = formattedArrayOfWrestlersTeamsAndSeeds[x + 1];
-        formattedArrayOfWrestlersTeamsAndSeeds[x] = placeholder2;
-        formattedArrayOfWrestlersTeamsAndSeeds[x + 1] = placeholder1;
+    for (x = 0; x < initialArrayOfCompetitorsOrderedBySeed.length; x++) {
+      if (x % 4 === 0 || x % 4 === 3) {
+        topArray.push(initialArrayOfCompetitorsOrderedBySeed[x]);
+      } else {
+        botArray.push(initialArrayOfCompetitorsOrderedBySeed[x]);
       }
     }
+
+    if (botArray.length === 2) {
+      bigArray.push(topArray);
+      bigArray.push(botArray);
+
+      return;
+    } else {
+      binarySplitting(topArray);
+      binarySplitting(botArray);
+    }
+  };
+  binarySplitting(initialArrayOfCompetitorsOrderedBySeed);
+  let bo2Array = [];
+  let newBiggestArray = [];
+
+  for (let a = 0; a < bigArray.length / 2; a++) {
+    bo2Array.unshift(bigArray[a + bigArray.length / 2]);
   }
+  for (let a = 0; a < bigArray.length / 2; a++) {
+    newBiggestArray.push(bigArray[a]);
+  }
+  for (let a = 0; a < bigArray.length / 2; a++) {
+    newBiggestArray.push(bo2Array[a]);
+  }
+
+  let extraNewBiggestArray = [];
+
+  for (let x = 0; x < newBiggestArray.length; x++) {
+    extraNewBiggestArray.push(newBiggestArray[x][0]);
+
+    extraNewBiggestArray.push(newBiggestArray[x][1]);
+  }
+
+  let makeSureOddSeedsAreOnTopFunction = (
+    formattedArrayOfWrestlersTeamsAndSeeds
+  ) => {
+    for (x = 0; x < formattedArrayOfWrestlersTeamsAndSeeds.length; x++) {
+      if (x % 2 === 0) {
+        if (formattedArrayOfWrestlersTeamsAndSeeds[x].seed % 2 === 0) {
+          let placeholder1 = formattedArrayOfWrestlersTeamsAndSeeds[x];
+          let placeholder2 = formattedArrayOfWrestlersTeamsAndSeeds[x + 1];
+          formattedArrayOfWrestlersTeamsAndSeeds[x] = placeholder2;
+          formattedArrayOfWrestlersTeamsAndSeeds[x + 1] = placeholder1;
+        }
+      }
+    }
+    return formattedArrayOfWrestlersTeamsAndSeeds;
+  };
+
+  console.log(makeSureOddSeedsAreOnTopFunction(extraNewBiggestArray));
+  let Output = makeSureOddSeedsAreOnTopFunction(extraNewBiggestArray);
+  return Output;
 };
 
-console.log(makeSureOddSeedsAreOnTopFunction(extraNewBiggestArray));
+handleSeedingFunction(people);
--- src/server/bracketing_logic/bracketBuilder.js
+++ src/server/bracketing_logic/bracketBuilder.js
@@ -1,3 +1,5 @@
+//The bracket builder
+
 // This is the data formatter. It will start by taking a tab separated list (in order of seeding from first to last) and maninpulating it into an array of objects where each object contains the wrestler's name and team name.
 let dataFormatter = (tabSeparatedWrestlersString) => {
   let arrayOfWrestlersAndTeamsTabSeparated = tabSeparatedWrestlersString.split(
@@ -51,22 +53,74 @@ Wrestler Seeded32	Team 32`);
 
 console.log({ formattedArrayOfWrestlersAndTeams });
 
-let makeSureOddSeedsAreOnTopFunction = (
-  formattedArrayOfWrestlersTeamsAndSeeds
-) => {
-  for (x = 0; x < formattedArrayOfWrestlersTeamsAndSeeds.length; x++) {
-    if (x % 2 === 0) {
-      if (formattedArrayOfWrestlersTeamsAndSeeds[x].seed % 2 === 0) {
-        let placeholder1 = formattedArrayOfWrestlersTeamsAndSeeds[x];
-        let placeholder2 = formattedArrayOfWrestlersTeamsAndSeeds[x + 1];
-        formattedArrayOfWrestlersTeamsAndSeeds[x] = placeholder2;
-        formattedArrayOfWrestlersTeamsAndSeeds[x + 1] = placeholder1;
+let handleSeedingFunction = (initialArrayOfCompetitorsOrderedBySeed) => {
+  let bigArray = [];
+  let binarySplitting = (initialArrayOfCompetitorsOrderedBySeed) => {
+    let topArray = [];
+    let botArray = [];
+
+    for (x = 0; x < initialArrayOfCompetitorsOrderedBySeed.length; x++) {
+      if (x % 4 === 0 || x % 4 === 3) {
+        topArray.push(initialArrayOfCompetitorsOrderedBySeed[x]);
+      } else {
+        botArray.push(initialArrayOfCompetitorsOrderedBySeed[x]);
       }
     }
+
+    if (botArray.length === 2) {
+      bigArray.push(topArray);
+      bigArray.push(botArray);
+
+      return;
+    } else {
+      binarySplitting(topArray);
+      binarySplitting(botArray);
+    }
+  };
+  binarySplitting(initialArrayOfCompetitorsOrderedBySeed);
+  let bo2Array = [];
+  let newBiggestArray = [];
+
+  for (let a = 0; a < bigArray.length / 2; a++) {
+    bo2Array.unshift(bigArray[a + bigArray.length / 2]);
   }
+  for (let a = 0; a < bigArray.length / 2; a++) {
+    newBiggestArray.push(bigArray[a]);
+  }
+  for (let a = 0; a < bigArray.length / 2; a++) {
+    newBiggestArray.push(bo2Array[a]);
+  }
+
+  let extraNewBiggestArray = [];
+
+  for (let x = 0; x < newBiggestArray.length; x++) {
+    extraNewBiggestArray.push(newBiggestArray[x][0]);
+
+    extraNewBiggestArray.push(newBiggestArray[x][1]);
+  }
+
+  let makeSureOddSeedsAreOnTopFunction = (
+    formattedArrayOfWrestlersTeamsAndSeeds
+  ) => {
+    for (x = 0; x < formattedArrayOfWrestlersTeamsAndSeeds.length; x++) {
+      if (x % 2 === 0) {
+        if (formattedArrayOfWrestlersTeamsAndSeeds[x].seed % 2 === 0) {
+          let placeholder1 = formattedArrayOfWrestlersTeamsAndSeeds[x];
+          let placeholder2 = formattedArrayOfWrestlersTeamsAndSeeds[x + 1];
+          formattedArrayOfWrestlersTeamsAndSeeds[x] = placeholder2;
+          formattedArrayOfWrestlersTeamsAndSeeds[x + 1] = placeholder1;
+        }
+      }
+    }
+    return formattedArrayOfWrestlersTeamsAndSeeds;
+  };
+
+  //console.log(makeSureOddSeedsAreOnTopFunction(extraNewBiggestArray));
+  let Output = makeSureOddSeedsAreOnTopFunction(extraNewBiggestArray);
+  return Output;
 };
 
-let seededArrayofWrestlersandTeams = makeSureOddSeedsAreOnTopFunction(
+let seededArrayofWrestlersAndTeams = handleSeedingFunction(
   formattedArrayOfWrestlersAndTeams
 );
 
@@ -716,4 +770,4 @@ let bracketBuilder = (formattedArrayOfWrestlersAndTeams) => {
   return populateBrackets(formattedArrayOfWrestlersAndTeams);
 };
 
-bracketBuilder(formattedArrayOfWrestlersAndTeams);
+bracketBuilder(seededArrayofWrestlersAndTeams);
