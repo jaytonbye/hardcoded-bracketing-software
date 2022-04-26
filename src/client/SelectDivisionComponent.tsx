4cda4c94f3ad0ec49e321bcef73145f84417f22b
Harry Corradi <harrycorradi@protonmail.com>
2022-04-26 11:26:44 -0400

    publishing to heroku

--- src/client/SelectDivisionComponent.tsx
+++ src/client/SelectDivisionComponent.tsx
@@ -29,11 +29,11 @@ export default function SelectDivisionComponent(props: any) {
     <>
       <h2>Select the division you would like to view:</h2>
       <select onChange={onEventChange}>
-<<<<<<< HEAD
-        <option>Select a division</option>
-=======
+{/* <<<<<<< HEAD
+        <option>Select a division</option> */}
+{/* ======= */}
         <option value="">Select a division</option>
->>>>>>> waynesBranch
+{/* >>>>>>> waynesBranch */}
         {allDivisions.map((division) => {
           return (
             <option key={division.id} value={division.id}>
