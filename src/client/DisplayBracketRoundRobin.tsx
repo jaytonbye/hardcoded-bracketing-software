b7934970e1e3a7c991a08ebdbd32879a64769a19
Harry Corradi <harrycorradi@protonmail.com>
2022-06-20 14:29:54 -0400

    from harry. all i did was git pull and then it made me push to github after that. wtf

--- src/client/DisplayBracketRoundRobin.tsx
+++ src/client/DisplayBracketRoundRobin.tsx
@@ -7,27 +7,29 @@ import SingleBoutForDisplayBracket from "./SingleBoutForDisplayBracket";
 import "./PracticingWithFlexboxToDisplayTheBrackets.scss";
 
 export default function DisplayBracketRoundRobin(props: any) {
-    const [bouts, setBouts] = React.useState([]);
-    const [dispatchToMat, setDispatchToMat] = React.useState();
-    const [modalShow, setModalShow] = React.useState<any>({});
+  const [bouts, setBouts] = React.useState([]);
+  const [dispatchToMat, setDispatchToMat] = React.useState();
+  const [modalShow, setModalShow] = React.useState<any>({});
 
-    let eventID = props.eventID;
-    let divisionID = props.divisionID;
+  let eventID = props.eventID;
+  let divisionID = props.divisionID;
 
-    React.useEffect(() => {
-        fetch(`/api/bouts/boutsByEventAndDivision/${eventID}&${divisionID}`)
-            .then((res) => res.json())
-            .then((results) => {
-                setBouts(results);
-            });
-    }, []);
+  React.useEffect(() => {
+    fetch(`/api/bouts/boutsByEventAndDivision/${eventID}&${divisionID}`)
+      .then((res) => res.json())
+      .then((results) => {
+        setBouts(results);
+      });
+  }, []);
 
-    return (false ? <h1>Round Robin Bracket</h1> :
-        <>
-            <UpdateAllByes divisionID={divisionID} eventID={eventID} />
-            <h3>Brackets go here:</h3>
+  return false ? (
+    <h1>Round Robin Bracket</h1>
+  ) : (
+    <>
+      <UpdateAllByes divisionID={divisionID} eventID={eventID} />
+      <h3>Brackets go here:</h3>
 
-            {/* {bouts.map((bout, index) => {
+      {/* {bouts.map((bout, index) => {
         //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
         const cardClasses = classNames("my-2", "mx-1", "col-10", {
           "the-card-border": bout.id % 2 === 0,
@@ -44,167 +46,179 @@ export default function DisplayBracketRoundRobin(props: any) {
           </>
         );
       })} */}
-            <div className="div-for-bracket_header-and-full_bracket">
-                <div className="bracket_header">
-
-                </div>
-                <div className="full_bracket">
-                    <div className="round">
-                        <h2 className="text-center" style={{ textDecoration: "underline" }}>Round 1</h2>
-                        {bouts.map((bout, index) => {
-                            //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
-                            const cardClasses = classNames("my-2", "mx-1", "col-10", {
-                                "the-card-border": bout.id % 2 === 0,
-                                "the-card-border-black": bout.id % 2 !== 0,
-                            });
-                            if (bout.round == 1) {
-                                return (
-                                    <div>
-                                        <SingleBoutForDisplayBracket
-                                            eventID={eventID}
-                                            bout={bout}
-                                            index={index}
-                                            bouts={bouts}
-                                        />
-                                    </div>
-                                );
-                            }
-                        })}
-                    </div>
-                    <div className="round">
-                        <h2 className="text-center" style={{ textDecoration: "underline" }}>Round 2</h2>
-                        {bouts.map((bout, index) => {
-                            //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
-                            const cardClasses = classNames("my-2", "mx-1", "col-10", {
-                                "the-card-border": bout.id % 2 === 0,
-                                "the-card-border-black": bout.id % 2 !== 0,
-                            });
-                            if (bout.round == 2) {
-                                return (
-                                    <div>
-                                        <SingleBoutForDisplayBracket
-                                            eventID={eventID}
-                                            bout={bout}
-                                            index={index}
-                                            bouts={bouts}
-                                        />
-                                    </div>
-                                );
-                            }
-                        })}
-                    </div>
-                    <div className="round">
-                        <h2 className="text-center" style={{ textDecoration: "underline" }}>Round 3</h2>
-                        {bouts.map((bout, index) => {
-                            //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
-                            const cardClasses = classNames("my-2", "mx-1", "col-10", {
-                                "the-card-border": bout.id % 2 === 0,
-                                "the-card-border-black": bout.id % 2 !== 0,
-                            });
-                            if (bout.round == 3) {
-                                return (
-                                    <div>
-                                        <SingleBoutForDisplayBracket
-                                            eventID={eventID}
-                                            bout={bout}
-                                            index={index}
-                                            bouts={bouts}
-                                        />
-                                    </div>
-                                );
-                            }
-                        })}
-                    </div>
-                    <div className="round">
-                        <h2 className="text-center" style={{ textDecoration: "underline" }}>Round 4</h2>
-                        {bouts.map((bout, index) => {
-                            //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
-                            const cardClasses = classNames("my-2", "mx-1", "col-10", {
-                                "the-card-border": bout.id % 2 === 0,
-                                "the-card-border-black": bout.id % 2 !== 0,
-                            });
-                            if (bout.round == 4) {
-                                return (
-                                    <div>
-                                        <SingleBoutForDisplayBracket
-                                            eventID={eventID}
-                                            bout={bout}
-                                            index={index}
-                                            bouts={bouts}
-                                        />
-                                    </div>
-                                );
-                            }
-                        })}
-                    </div>
-                    <div className="round">
-                        <h2 className="text-center" style={{ textDecoration: "underline" }}>Round 5</h2>
-                        {bouts.map((bout, index) => {
-                            //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
-                            const cardClasses = classNames("my-2", "mx-1", "col-10", {
-                                "the-card-border": bout.id % 2 === 0,
-                                "the-card-border-black": bout.id % 2 !== 0,
-                            });
-                            if (bout.round == 5) {
-                                return (
-                                    <div>
-                                        <SingleBoutForDisplayBracket
-                                            eventID={eventID}
-                                            bout={bout}
-                                            index={index}
-                                            bouts={bouts}
-                                        />
-                                    </div>
-                                );
-                            }
-                        })}
-                    </div>
-                    <div className="round">
-                        <h2 className="text-center" style={{ textDecoration: "underline" }}>Round 6</h2>
-                        {bouts.map((bout, index) => {
-                            //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
-                            const cardClasses = classNames("my-2", "mx-1", "col-10", {
-                                "the-card-border": bout.id % 2 === 0,
-                                "the-card-border-black": bout.id % 2 !== 0,
-                            });
-                            if (bout.round == 6) {
-                                return (
-                                    <div>
-                                        <SingleBoutForDisplayBracket
-                                            eventID={eventID}
-                                            bout={bout}
-                                            index={index}
-                                            bouts={bouts}
-                                        />
-                                    </div>
-                                );
-                            }
-                        })}
-                    </div>
-                    <div className="round">
-                        <h2 className="text-center" style={{ textDecoration: "underline" }}>Round 7</h2>
-                        {bouts.map((bout, index) => {
-                            //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
-                            const cardClasses = classNames("my-2", "mx-1", "col-10", {
-                                "the-card-border": bout.id % 2 === 0,
-                                "the-card-border-black": bout.id % 2 !== 0,
-                            });
-                            if (bout.round == 7) {
-                                return (
-                                    <div>
-                                        <SingleBoutForDisplayBracket
-                                            eventID={eventID}
-                                            bout={bout}
-                                            index={index}
-                                            bouts={bouts}
-                                        />
-                                    </div>
-                                );
-                            }
-                        })}
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+      <div className="div-for-bracket_header-and-full_bracket">
+        <div className="bracket_header"></div>
+        <div className="full_bracket">
+          <div className="round">
+            <h2 className="text-center" style={{ textDecoration: "underline" }}>
+              Round 1
+            </h2>
+            {bouts.map((bout, index) => {
+              //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
+              const cardClasses = classNames("my-2", "mx-1", "col-10", {
+                "the-card-border": bout.id % 2 === 0,
+                "the-card-border-black": bout.id % 2 !== 0,
+              });
+              if (bout.round == 1) {
+                return (
+                  <div>
+                    <SingleBoutForDisplayBracket
+                      eventID={eventID}
+                      bout={bout}
+                      index={index}
+                      bouts={bouts}
+                    />
+                  </div>
+                );
+              }
+            })}
+          </div>
+          <div className="round">
+            <h2 className="text-center" style={{ textDecoration: "underline" }}>
+              Round 2
+            </h2>
+            {bouts.map((bout, index) => {
+              //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
+              const cardClasses = classNames("my-2", "mx-1", "col-10", {
+                "the-card-border": bout.id % 2 === 0,
+                "the-card-border-black": bout.id % 2 !== 0,
+              });
+              if (bout.round == 2) {
+                return (
+                  <div>
+                    <SingleBoutForDisplayBracket
+                      eventID={eventID}
+                      bout={bout}
+                      index={index}
+                      bouts={bouts}
+                    />
+                  </div>
+                );
+              }
+            })}
+          </div>
+          <div className="round">
+            <h2 className="text-center" style={{ textDecoration: "underline" }}>
+              Round 3
+            </h2>
+            {bouts.map((bout, index) => {
+              //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
+              const cardClasses = classNames("my-2", "mx-1", "col-10", {
+                "the-card-border": bout.id % 2 === 0,
+                "the-card-border-black": bout.id % 2 !== 0,
+              });
+              if (bout.round == 3) {
+                return (
+                  <div>
+                    <SingleBoutForDisplayBracket
+                      eventID={eventID}
+                      bout={bout}
+                      index={index}
+                      bouts={bouts}
+                    />
+                  </div>
+                );
+              }
+            })}
+          </div>
+          <div className="round">
+            <h2 className="text-center" style={{ textDecoration: "underline" }}>
+              Round 4
+            </h2>
+            {bouts.map((bout, index) => {
+              //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
+              const cardClasses = classNames("my-2", "mx-1", "col-10", {
+                "the-card-border": bout.id % 2 === 0,
+                "the-card-border-black": bout.id % 2 !== 0,
+              });
+              if (bout.round == 4) {
+                return (
+                  <div>
+                    <SingleBoutForDisplayBracket
+                      eventID={eventID}
+                      bout={bout}
+                      index={index}
+                      bouts={bouts}
+                    />
+                  </div>
+                );
+              }
+            })}
+          </div>
+          <div className="round">
+            <h2 className="text-center" style={{ textDecoration: "underline" }}>
+              Round 5
+            </h2>
+            {bouts.map((bout, index) => {
+              //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
+              const cardClasses = classNames("my-2", "mx-1", "col-10", {
+                "the-card-border": bout.id % 2 === 0,
+                "the-card-border-black": bout.id % 2 !== 0,
+              });
+              if (bout.round == 5) {
+                return (
+                  <div>
+                    <SingleBoutForDisplayBracket
+                      eventID={eventID}
+                      bout={bout}
+                      index={index}
+                      bouts={bouts}
+                    />
+                  </div>
+                );
+              }
+            })}
+          </div>
+          <div className="round">
+            <h2 className="text-center" style={{ textDecoration: "underline" }}>
+              Round 6
+            </h2>
+            {bouts.map((bout, index) => {
+              //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
+              const cardClasses = classNames("my-2", "mx-1", "col-10", {
+                "the-card-border": bout.id % 2 === 0,
+                "the-card-border-black": bout.id % 2 !== 0,
+              });
+              if (bout.round == 6) {
+                return (
+                  <div>
+                    <SingleBoutForDisplayBracket
+                      eventID={eventID}
+                      bout={bout}
+                      index={index}
+                      bouts={bouts}
+                    />
+                  </div>
+                );
+              }
+            })}
+          </div>
+          <div className="round">
+            <h2 className="text-center" style={{ textDecoration: "underline" }}>
+              Round 7
+            </h2>
+            {bouts.map((bout, index) => {
+              //I'm not sure where this logic belongs. I would imagine it belongs in the SingleBoutForDisplayBracket component. Ask wc.
+              const cardClasses = classNames("my-2", "mx-1", "col-10", {
+                "the-card-border": bout.id % 2 === 0,
+                "the-card-border-black": bout.id % 2 !== 0,
+              });
+              if (bout.round == 7) {
+                return (
+                  <div>
+                    <SingleBoutForDisplayBracket
+                      eventID={eventID}
+                      bout={bout}
+                      index={index}
+                      bouts={bouts}
+                    />
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
